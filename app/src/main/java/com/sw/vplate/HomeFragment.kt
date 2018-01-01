badfc6bed2953fc57ca0fc2c1549fd0861e7a818
구설원 <seolwon6289@naver.com>
2018-01-01 17:28:55 +0900

    20180101_1728_Tab형성

---
+++ app/src/main/java/com/sw/vplate/HomeFragment.kt
@@ -0,0 +1,9 @@
+package com.sw.vplate
+
+import android.support.v4.app.Fragment
+
+/**
+ * Created by SM-PC on 2018-01-01.
+ */
+class HomeFragment: Fragment() {
+}
---
+++ app/src/main/java/com/sw/vplate/VideoViewHolder.kt
@@ -0,0 +1,17 @@
+package com.sw.vplate
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+
+/**
+ * Created by SM-PC on 2018-01-01.
+ */
+class VideoViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
+    var VideoImage : ImageView = itemView!!.findViewById(R.id.item_image) as ImageView
+    var VideoNamge : TextView = itemView!!.findViewById(R.id.itemTitle) as TextView
+    var VideoStory : TextView = itemView!!.findViewById(R.id.itemStory) as TextView
+    var VideoTime : TextView = itemView!!.findViewById(R.id.itemTime) as TextView
+
+}
