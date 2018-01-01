9c20ac4423ab0c3d54b4b49a8fa271abd261ec9e
구설원 <seolwon6289@naver.com>
2018-01-01 16:53:20 +0900

    20180101_1652_item.class 생성

---
+++ app/src/main/java/com/sw/vplate/VideoItem.kt
@@ -0,0 +1,11 @@
+package com.sw.vplate
+
+/**
+ * Created by SM-PC on 2018-01-01.
+ */
+data class VideoItem (
+        var VideoImage : Int,
+        var VideoName :String,
+        var VideoStory : String,
+        var VideoTime : String
+)
