224301100d61c0eaf78d214ea83497ee301435dd
구설원 <seolwon6289@naver.com>
2018-01-03 12:39:24 +0900

    20180103_1237_package이름변경

--- app/src/main/java/com/vplate/TabAdapter.kt
+++ app/src/main/java/com/vplate/TabAdapter.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentStatePagerAdapter
-import com.sw.vplate.Fragment.*
 import com.vplate.Fragment.*
 
 /**
