5976321b8083c4a3307df62fa60a0bfdc8d1c575
soomin <vywlgns1102@naver.com>
2018-01-03 13:24:22 +0900

    20180103_1323_메인

---
+++ app/src/main/java/com/vplate/SplashActivity.kt
@@ -0,0 +1,13 @@
+package com.vplate
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.sw.vplate.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
