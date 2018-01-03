bf78cd854f53151b0b1ba766d427fd2ef9f744aa
soomin <vywlgns1102@naver.com>
2018-01-03 13:49:18 +0900

    20180103_1349_메인로그인

--- app/src/main/java/com/vplate/JoinActivity.kt
+++ app/src/main/java/com/vplate/JoinActivity.kt
@@ -1,12 +1,12 @@
-package com.sw.vplate
+package com.vplate
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class SplashActivity : AppCompatActivity() {
+class JoinActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        setContentView(R.layout.activity_join)
     }
 }
--- app/src/main/java/com/vplate/SplashActivity.kt
+++ app/src/main/java/com/vplate/SplashActivity.kt
@@ -1,13 +1,14 @@
 package com.vplate
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.sw.vplate.R
+import android.support.v7.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+
     }
 }
