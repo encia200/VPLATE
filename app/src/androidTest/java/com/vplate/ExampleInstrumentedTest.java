72ed5b951dbeecb2b098b6b02d286c89b1a85bfa
구설원 <seolwon6289@naver.com>
2018-01-03 13:21:44 +0900

    20180103_1319_package이름변경

--- app/src/androidTest/java/com/vplate/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/vplate/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.sw.vplate;
+package com.vplate;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.sw.vplate", appContext.getPackageName());
+        assertEquals("com.vplate", appContext.getPackageName());
     }
 }
