69b4bd5514c502ab7b7c32fa9eb63a783503007d
encia200 <encia200@gmail.com>
2018-01-08 04:30:28 +0900

    20180107_2315 홈화면 네트워크_디자인 수정해야함

--- app/src/main/java/com/vplate/activity/LoginMainActivity.kt
+++ app/src/main/java/com/vplate/activity/LoginMainActivity.kt
@@ -3,8 +3,8 @@ package com.vplate.activity
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.Button
 import com.vplate.R
+import kotlinx.android.synthetic.main.activity_loginmain.*
 
 class LoginMainActivity : AppCompatActivity() {
 
@@ -13,15 +13,14 @@ class LoginMainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_loginmain)
 
         // 회원가입 버튼
-        var joinBtn: Button = findViewById(R.id.splash_joinBtn) as Button
-        joinBtn!!.setOnClickListener {
+
+        splash_joinBtn!!.setOnClickListener {
             val intent = Intent(applicationContext, JoinActivity::class.java)
             startActivity(intent)
         }
 
         // 로그인버튼
-        var loginBtn: Button = findViewById(R.id.splash_loginBtn) as Button
-        loginBtn!!.setOnClickListener {
+        splash_loginBtn!!.setOnClickListener {
             val intent = Intent(applicationContext, LoginActivity::class.java)
             startActivity(intent)
         }
